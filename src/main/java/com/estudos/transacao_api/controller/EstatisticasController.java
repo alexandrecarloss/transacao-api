1553a3f79be5bc1ee2fc5fbd0ca4707cf29cf0a5
Carlos Alexandre <karurosualexandresouza123@gmail.com>
2026-01-18 23:22:20 -0400

    Controller de estatísticas

---
+++ src/main/java/com/estudos/transacao_api/controller/EstatisticasController.java
@@ -0,0 +1,25 @@
+package com.estudos.transacao_api.controller;
+
+import com.estudos.transacao_api.business.services.EstatisticasService;
+import com.estudos.transacao_api.controller.dtos.EstatisticasResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estatistica")
+@RequiredArgsConstructor
+public class EstatisticasController {
+    private final EstatisticasService estatisticasService;
+
+    public EstatisticasController(EstatisticasService estatisticasService) {
+        this.estatisticasService = estatisticasService;
+    }
+
+    public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
+            @RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") Integer interValoBusca) {
+        return ResponseEntity.ok(estatisticasService.calcularEstatisticasTransacoes(interValoBusca));
+    }
+}
